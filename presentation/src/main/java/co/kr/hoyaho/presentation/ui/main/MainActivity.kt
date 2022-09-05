f946b019baceeea659db37ed1900bc8eef768b5c
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-05 22:39:17 +0900

    feat : 프레젠테이션 모듈 생성

---
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package co.kr.hoyaho.presentation.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.kr.hoyaho.presentation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
