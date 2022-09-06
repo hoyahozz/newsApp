1cfe83d6f52348886550016506fd431213835fd9
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-06 21:07:50 +0900

    feat : 메인 액티비티에 바텀 네비게이션 적용

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/main/MainActivity.kt
@@ -1,12 +1,27 @@
 package co.kr.hoyaho.presentation.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import co.kr.hoyaho.presentation.R
+import co.kr.hoyaho.presentation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.setGraph(R.navigation.wanted_navigation)
+        binding.mainNvBottom.setupWithNavController(navController)
     }
 }
