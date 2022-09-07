1bc1036b7575853d1359097a378bafcc9597df27
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-07 10:27:28 +0900

    feat : 휘하 프래그먼트에서 툴바 상태를 변경하도록 설정

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/category/CategoryFragment.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/category/CategoryFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import co.kr.hoyaho.presentation.R
 import co.kr.hoyaho.presentation.databinding.FragmentCategoryBinding
+import co.kr.hoyaho.presentation.ui.main.MainViewModel
 
 class CategoryFragment : Fragment() {
 
     private var _binding: FragmentCategoryBinding? = null
     private val binding get() = _binding!!
+
+    private val sharedViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +25,10 @@ class CategoryFragment : Fragment() {
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_category, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        sharedViewModel.updateToolbarState(getString(R.string.category_toolbar_title), false)
     }
 
     override fun onDestroyView() {
--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/save/SaveFragment.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/save/SaveFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import co.kr.hoyaho.presentation.R
 import co.kr.hoyaho.presentation.databinding.FragmentSaveBinding
+import co.kr.hoyaho.presentation.ui.main.MainViewModel
 
 class SaveFragment : Fragment() {
 
     private var _binding: FragmentSaveBinding? = null
     private val binding get() = _binding!!
+
+    private val sharedViewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,10 @@ class SaveFragment : Fragment() {
     ): View? {
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_save, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        sharedViewModel.updateToolbarState(getString(R.string.save_toolbar_title), false)
     }
 
     override fun onDestroyView() {
