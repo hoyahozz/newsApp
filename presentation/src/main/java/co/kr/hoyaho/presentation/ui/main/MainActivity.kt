4ac52c2c526b2aacc72ce2caee17ce99c0372459
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-07 10:26:37 +0900

    feat : 메인 액티비티와 베이스 툴바 연동

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package co.kr.hoyaho.presentation.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -13,9 +14,14 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.main_nav_container) as NavHostFragment
@@ -23,5 +29,9 @@ class MainActivity : AppCompatActivity() {
 
         navController.setGraph(R.navigation.wanted_navigation)
         binding.mainNvBottom.setupWithNavController(navController)
+
+        binding.mainToolbar.back.setOnClickListener {
+            navController.popBackStack()
+        }
     }
 }
