6b43a5e7af08872372691c202a38c1ef11dd6d5c
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-09 18:24:09 +0900

    feat : HiltAndroidApp 선언

--- app/src/main/java/co/kr/hoyaho/WantedApplication.kt
+++ app/src/main/java/co/kr/hoyaho/WantedApplication.kt
@@ -1,7 +1,9 @@
 package co.kr.hoyaho
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class WantedApplication : Application() {
     override fun onCreate() {
         super.onCreate()
