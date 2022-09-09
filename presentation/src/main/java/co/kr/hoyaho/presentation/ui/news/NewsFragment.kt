9ee78e3d6b6e65223fcde39dd9565c2d17308961
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-09 22:11:47 +0900

    feat : 뉴스 탭 리사이클러뷰 연동 및 API 적용

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/news/NewsFragment.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/news/NewsFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import co.kr.hoyaho.presentation.R
 import co.kr.hoyaho.presentation.databinding.FragmentNewsBinding
+import co.kr.hoyaho.presentation.ui.adapter.NewsAdapter
 import co.kr.hoyaho.presentation.ui.main.MainViewModel
+import co.kr.hoyaho.presentation.ui.util.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,7 +23,9 @@ class NewsFragment : Fragment() {
     private var _binding: FragmentNewsBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: NewsViewModel by viewModels()
     private val sharedViewModel: MainViewModel by activityViewModels()
+    private lateinit var adapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,6 +38,22 @@ class NewsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         sharedViewModel.updateToolbarState(getString(R.string.news_toolbar_title), false)
+
+        adapter = NewsAdapter()
+
+        binding.newsRcv.apply {
+            this.adapter = this@NewsFragment.adapter
+            this.layoutManager = LinearLayoutManager(requireActivity())
+            this.setHasFixedSize(true)
+        }
+        
+        viewModel.showToast.observe(viewLifecycleOwner, EventObserver { msg ->
+            Toast.makeText(requireActivity(), msg, Toast.LENGTH_SHORT).show()
+        })
+
+        viewModel.news.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
     }
 
     override fun onDestroyView() {
