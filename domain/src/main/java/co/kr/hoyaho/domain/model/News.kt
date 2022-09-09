5579dfc17be647f94d6fb822ce281505aad673f8
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-09 18:44:45 +0900

    fix : 뉴스 도메인 모델 변경

--- domain/src/main/java/co/kr/hoyaho/domain/model/News.kt
+++ domain/src/main/java/co/kr/hoyaho/domain/model/News.kt
@@ -2,14 +2,16 @@ package co.kr.hoyaho.domain.model
 
 /**
  * @property title : 뉴스 제목
- * @property imgUrl : 뉴스 URL
- * @property writer : 뉴스 작성자
- * @property time : 뉴스 작성 시간
+ * @property author : 뉴스 작성자
+ * @property imgUrl : 뉴스 이미지 URL
+ * @property elapsed : 뉴스 작성 후 경과 시간
+ * @property content : 뉴스 본문
  */
 
 data class News(
     val title: String,
+    val author: String,
     val imgUrl: String,
-    val writer: String,
-    val time: String
+    val elapsed: String,
+    val content: String
 )
