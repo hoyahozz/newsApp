6e4b3eb4f7a08e0f3d4c09640a386f7398165d48
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-09 20:28:26 +0900

    feat : 에러 타입에 따라 적합한 메시지를 반환하는 확장 함수 추가

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/util/Extends.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/util/Extends.kt
@@ -1,6 +1,26 @@
 package co.kr.hoyaho.presentation.ui.util
 
 import android.content.res.Resources
+import co.kr.hoyaho.domain.NetworkError
+import co.kr.hoyaho.presentation.R
 
 val Int.dp: Int
     get() = (this * Resources.getSystem().displayMetrics.density + 0.5f).toInt()
+
+fun NetworkError.toErrorMessage(
+): String {
+    return when (this) {
+        is NetworkError.Unknown -> {
+            Resources.getSystem().getString(R.string.error_unknown)
+        }
+        is NetworkError.Timeout -> {
+            Resources.getSystem().getString(R.string.error_timeout)
+        }
+        is NetworkError.InternalServer -> {
+            Resources.getSystem().getString(R.string.error_internal_server)
+        }
+        is NetworkError.BadRequest -> {
+            "Error Code : $code, Invalid request.\nPlease run again in a few minutes."
+        }
+    }
+}
