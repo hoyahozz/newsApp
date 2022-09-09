a8f232824a20eb8cba843b46463be878b3e06db1
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-09 22:12:02 +0900

    feat : 메인 뷰모델에 HiltViewModel 선언

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/main/MainViewModel.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/main/MainViewModel.kt
@@ -3,8 +3,11 @@ package co.kr.hoyaho.presentation.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
 
     private val _toolbarState: MutableLiveData<Pair<String, Boolean>> = MutableLiveData()
     val toolbarState: LiveData<Pair<String, Boolean>> get() = _toolbarState
