a3e2cf3c3eb2afcf7325c85022be1933f5b231f1
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-10 22:51:59 +0900

    feat : 뉴스 -> 디테일 화면 이동 구현

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/adapter/NewsAdapter.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/adapter/NewsAdapter.kt
@@ -2,10 +2,12 @@ package co.kr.hoyaho.presentation.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import co.kr.hoyaho.domain.model.News
 import co.kr.hoyaho.presentation.R
 import co.kr.hoyaho.presentation.databinding.ItemNewsBinding
+import co.kr.hoyaho.presentation.ui.news.NewsFragmentDirections
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -27,12 +29,17 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
             binding.writer.text = item.author
             binding.time.text = item.elapsed
 
+            // TODO : Glide 에서 Error 는 transform 을 지원하지 않음
             Glide.with(binding.root.context)
                 .load(item.imgUrl)
                 .placeholder(R.color.gray)
-                .transform(CenterCrop(), RoundedCorners(4))
                 .error(R.drawable.img_not_found)
+                .transform(CenterCrop(), RoundedCorners(4))
                 .into(binding.thumbnail)
+
+            binding.container.setOnClickListener { view ->
+                view.findNavController().navigate(NewsFragmentDirections.actionNewsToDetail(item))
+            }
         }
     }
 
