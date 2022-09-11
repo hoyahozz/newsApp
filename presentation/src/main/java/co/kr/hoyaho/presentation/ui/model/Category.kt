d4a7ba15cfaa197df18e0b7e019831363f89f438
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-11 19:41:31 +0900

    feat : 카테고리 프레젠테이션 모델 클래스 추가

---
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/model/Category.kt
@@ -0,0 +1,13 @@
+package co.kr.hoyaho.presentation.ui.model
+
+import co.kr.hoyaho.presentation.R
+
+enum class Category(val image: Int) {
+    BUSINESS(R.drawable.ic_business),
+    ENTERTAINMENT(R.drawable.ic_entertainment),
+    GENERAL(R.drawable.ic_general),
+    HEALTH(R.drawable.ic_health),
+    SCIENCE(R.drawable.ic_science),
+    SPORTS(R.drawable.ic_sports),
+    TECHNOLOGY(R.drawable.ic_technology)
+}
