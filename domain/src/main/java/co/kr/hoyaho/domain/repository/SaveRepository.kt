c3e50889410120a6f24d0d3f7335efc5959e9c3d
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-11 21:17:30 +0900

    feat : 로컬 데이터 Repo 추가

---
+++ domain/src/main/java/co/kr/hoyaho/domain/repository/SaveRepository.kt
@@ -0,0 +1,8 @@
+package co.kr.hoyaho.domain.repository
+
+import co.kr.hoyaho.domain.model.News
+
+interface SaveRepository {
+    suspend fun getSavedNews(): List<News>
+    suspend fun insertSavedNews(news: News)
+}
