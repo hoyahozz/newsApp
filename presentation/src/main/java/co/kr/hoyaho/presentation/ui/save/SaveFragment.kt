340eed5fb539fb5cf0d724e694197f356a81f445
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-11 21:19:48 +0900

    feat : 저장된 목록을 화면에서 조회하도록 어댑터 연동

--- presentation/src/main/java/co/kr/hoyaho/presentation/ui/save/SaveFragment.kt
+++ presentation/src/main/java/co/kr/hoyaho/presentation/ui/save/SaveFragment.kt
@@ -7,18 +7,25 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.kr.hoyaho.domain.model.News
 import co.kr.hoyaho.presentation.R
 import co.kr.hoyaho.presentation.databinding.FragmentSaveBinding
+import co.kr.hoyaho.presentation.ui.adapter.NewsAdapter
+import co.kr.hoyaho.presentation.ui.adapter.NewsClickListener
 import co.kr.hoyaho.presentation.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SaveFragment : Fragment() {
+class SaveFragment : Fragment(), NewsClickListener {
 
     private var _binding: FragmentSaveBinding? = null
     private val binding get() = _binding!!
 
+    private val viewModel: SaveViewModel by viewModels()
     private val sharedViewModel: MainViewModel by activityViewModels()
+    private lateinit var adapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,10 +37,27 @@ class SaveFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         sharedViewModel.updateToolbarState(getString(R.string.save_toolbar_title), false)
+        viewModel.getSavedNews()
+
+        adapter = NewsAdapter().apply { setItemClickListener(this@SaveFragment) }
+
+        binding.saveRcv.apply {
+            this.adapter = this@SaveFragment.adapter
+            this.layoutManager = LinearLayoutManager(requireActivity())
+            this.setHasFixedSize(true)
+        }
+
+        viewModel.news.observe(viewLifecycleOwner) {
+            adapter.submitList(it)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun navigateToDetail(news: News) {
+        // TODO
     }
 }
