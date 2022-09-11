93fded4ae7497588321ae8e8bcc98d3187bc4e9e
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-11 21:18:45 +0900

    feat : SaveRepo Hilt 모듈 추가

--- data/src/main/java/co/kr/hoyaho/data/di/RepositoryModule.kt
+++ data/src/main/java/co/kr/hoyaho/data/di/RepositoryModule.kt
@@ -1,9 +1,12 @@
 package co.kr.hoyaho.data.di
 
+import co.kr.hoyaho.data.local.dao.SaveDao
 import co.kr.hoyaho.data.remote.api.NewsApi
 import co.kr.hoyaho.data.repository.NewsRepositoryImpl
+import co.kr.hoyaho.data.repository.SaveRepositoryImpl
 import co.kr.hoyaho.domain.interactor.NetworkErrorHandler
 import co.kr.hoyaho.domain.repository.NewsRepository
+import co.kr.hoyaho.domain.repository.SaveRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,5 +22,12 @@ object RepositoryModule {
         networkErrorHandler: NetworkErrorHandler
     ): NewsRepository {
         return NewsRepositoryImpl(api, networkErrorHandler)
+    }
+
+    @Provides
+    fun provideSaveRepository(
+        dao: SaveDao
+    ): SaveRepository {
+        return SaveRepositoryImpl(dao)
     }
 }
