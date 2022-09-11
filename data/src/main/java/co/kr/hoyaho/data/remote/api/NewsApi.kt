4ae817221b5e1c7242d86e58c688c09baef3d480
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-09-11 20:14:09 +0900

    feat : 카테고리별 뉴스 조회 API 추가

--- data/src/main/java/co/kr/hoyaho/data/remote/api/NewsApi.kt
+++ data/src/main/java/co/kr/hoyaho/data/remote/api/NewsApi.kt
@@ -2,9 +2,15 @@ package co.kr.hoyaho.data.remote.api
 
 import co.kr.hoyaho.data.remote.entity.NewsResponseHeader
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsApi {
 
     @GET("top-headlines")
     suspend fun getHeadLineNews(): NewsResponseHeader
+
+    @GET("top-headlines")
+    suspend fun getCategoryNews(
+        @Query("category") category: String
+    ): NewsResponseHeader
 }
